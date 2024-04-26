8f399acce1607884ad22c41e8125bf4d1327300a
Omar Mostafa <152614180+OMARMS11@users.noreply.github.com>
2024-04-26 15:45:19 +0300

    DFST Function is finished

--- Road-Map-Construction/RoadMapConstruction/graph.h
+++ Road-Map-Construction/RoadMapConstruction/graph.h
@@ -1,25 +1,35 @@
 #pragma once
 #include <unordered_map>
-#include<set>
 #include <list>
 #include <string>
 using namespace std;
 
-class Edge {
-	double length;
+class Edge 
+{
 	string vertex1, vertex2;
+public:
+	Edge(string, string, double);
+	Edge();
+	double length;
+
+	string getVertex1();
+	string getVertex2();
+
+	bool operator < (Edge e);
+	bool operator == (Edge e);
+
 };
 
 class Graph
 {
+public:
 	unordered_map<string, list<Edge>> adjacencyList;
 	
 	void insertVertex(string);
 	void insertEdge(string, string, double);
 	void deleteVertex(string);
 	void deleteEdge(string, string);
-	void DFStraversal(int v);
-	void DFStil(string v, set<string>& s);
+	void DFStraversal();
 	void BFStraversal();
 	void PrimMinimumSpanningTree();
 };
